44101719b9edb5eca70d5c70bd835bc1c87707a8
alagan1406 <alagan.nedunchezhian@cogninzant.com>
2022-02-25 12:34:51 +0000

    Push project content to github

---
+++ src/main/java/sample/spring/xsuaa/DataService.java
@@ -0,0 +1,32 @@
+/**
+ * SPDX-FileCopyrightText: 2018-2021 SAP SE or an SAP affiliate company and Cloud Security Client Java contributors
+ * 
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package sample.spring.xsuaa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Service;
+
+/**
+ * Simple DataLayer interface that shows how Spring global message security
+ * can be used to control access to data objects on a method level.
+ */
+@Service
+public class DataService {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * Reads sensitive data from the data layer.
+     * User requires scope {@code Admin}
+     * for this to succeed.
+     *
+     */
+    @PreAuthorize("hasAuthority('Admin')")
+    String readSensitiveData() {
+        logger.info("Reading sensitive data.");
+        return "You got the sensitive data";
+    }
+}
---
+++ src/main/java/sample/spring/xsuaa/SecurityConfiguration.java
@@ -0,0 +1,67 @@
+/**
+ * SPDX-FileCopyrightText: 2018-2021 SAP SE or an SAP affiliate company and Cloud Security Client Java contributors
+ * 
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package sample.spring.xsuaa;
+
+import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
+import com.sap.cloud.security.xsuaa.extractor.IasXsuaaExchangeBroker;
+import com.sap.cloud.security.xsuaa.tokenflows.XsuaaTokenFlows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import com.sap.cloud.security.xsuaa.token.TokenAuthenticationConverter;
+
+@Configuration
+@EnableWebSecurity(debug = true) // TODO "debug" may include sensitive information. Do not use in a production system!
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	XsuaaServiceConfiguration xsuaaServiceConfiguration;
+
+	@Autowired
+	XsuaaTokenFlows xsuaaTokenFlows;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session is created by approuter
+			.and()
+				.authorizeRequests()
+				.antMatchers("/v1/sayHello").hasAuthority("Read")
+				.antMatchers("/v1/*").authenticated()
+				.antMatchers("/v2/*").hasAuthority("Read")
+				.antMatchers("/v3/*").hasAuthority("Read")
+				.antMatchers("/v3/requestRefreshToken/*").hasAuthority("Read")
+				.antMatchers("/health").permitAll()
+				.anyRequest().denyAll()
+			.and()
+				.oauth2ResourceServer()
+				.bearerTokenResolver(new IasXsuaaExchangeBroker(xsuaaTokenFlows))
+				.jwt()
+				.jwtAuthenticationConverter(getJwtAuthenticationConverter());
+		// @formatter:on
+	}
+
+	/**
+	 * Customizes how GrantedAuthority are derived from a Jwt
+	 */
+	Converter<Jwt, AbstractAuthenticationToken> getJwtAuthenticationConverter() {
+		TokenAuthenticationConverter converter = new TokenAuthenticationConverter(xsuaaServiceConfiguration);
+		converter.setLocalScopeAsAuthorities(true);
+		return converter;
+	}
+
+}
